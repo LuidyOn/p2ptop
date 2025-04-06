8ff1790aee800bbc3f217612ea2b8d9646a9e0ad
MariaEduarda-Almeidaa <mariaeduardafer907@gmail.com>
2025-04-06 14:46:17 -0300

    Update p2p.java

--- p2p/p2p.java
+++ p2p/p2p.java
@@ -1,6 +1,20 @@
 public class p2p {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Qual é seu apelido? ");
+        String apelido = scanner.nextLine();
+
+        System.out.print("Qual é sua porta? ");
+        int minhaPorta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Qual é o IP? ");
+        String ip = scanner.nextLine();
+
+        System.out.print("Qual é a porta do outro cliente? ");
+        int portaOutro = scanner.nextInt();
+        scanner.nextLine();
     
     } 
 }
